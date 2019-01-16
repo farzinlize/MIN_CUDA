acf12b68a4282cc9831928657c0d5731887207c5
Farzin Mohammadi <farzinlize@live.com>
2019-01-16 20:14:45 +0330

    fix bugs and add DEBUG to make.bat

--- kernel.cu
+++ kernel.cu
@@ -11,6 +11,8 @@ void __syncthreads();
 #endif
 
 __device__	int device_min(int a, int b) {
+	if(a < 0 || b < 0)
+		return -1;
 	return a < b ? a : b;
 }
 
@@ -20,13 +22,14 @@ __global__ void minKernel(int *a_in, int *out){
 	unsigned int tid = blockDim.x * blockIdx.x + tid_block;
 	
 	a_s[tid_block] = a_in[tid];
+	__syncthreads();
 
 	for (unsigned int s = blockDim.x/2; s > 0 ; s >>= 1) {
 		if (tid_block < s)
 			a_s[tid_block] = device_min(a_s[tid_block], a_s[tid_block + s]);
 		__syncthreads();
 	}
-
+	//printf("%d %d\n" , tid , out[tid]);
 	if (tid_block == 0)
 		out[blockIdx.x] = a_s[0];
 }
@@ -41,12 +44,18 @@ int find_min_seq(int *a, int size) {
 }
 
 int main(){
+
 	int size = 1024 * 1024, block_size = 1024;
 	int *a_h, *a_d, *out_d, *device_out_h;
 
 	initialize_data_random(&a_h, size);
 	initialize_data_zero(&device_out_h, block_size);
 	
+	#ifdef DEBUG
+	printf("device_out_h array: \n");
+	for(int k=0;k<block_size;k++){printf("%d\t", device_out_h[k]);}
+	#endif
+
 	int min_seq = find_min_seq(a_h, size);
 
 	CUDA_CHECK_RETURN(cudaMalloc((void **)&a_d, sizeof(int)*size));
@@ -61,7 +70,7 @@ int main(){
 	CUDA_CHECK_RETURN(cudaGetLastError());
 
 	minKernel <<<grid_dim, block_dim, sizeof(int)*block_size, NULL >>> (a_d, out_d);
-
+	
 	CUDA_CHECK_RETURN(cudaDeviceSynchronize());
 	CUDA_CHECK_RETURN(cudaGetLastError());
 
@@ -69,9 +78,12 @@ int main(){
 	CUDA_CHECK_RETURN(cudaDeviceSynchronize());
 	CUDA_CHECK_RETURN(cudaGetLastError());
 
+	#ifdef DEBUG
+	printf("device_out_h array: \n");
 	for(int k=0; k < block_size ;k++){
 		printf("%d\t", device_out_h[k]);
 	}
+	#endif
 
 	int min_parralel = find_min_seq(device_out_h, block_size);
 
--- make.bat
+++ make.bat
@@ -1,8 +1,13 @@
 @echo off
 if "%1" == "clean" goto :clean
+if "%1" == "debug" goto :debug
 
 nvcc -o MIN_CUDA.exe kernel.cu helper_functions.c 
 goto :eof
 
+:debug
+nvcc -o MIN_CUDA_DEBUG.exe kernel.cu helper_functions.c -D DEBUG
+goto :eof
+
 :clean
-del MIN_CUDA.exe MIN_CUDA.exp MIN_CUDA.lib
+del MIN_CUDA.exe MIN_CUDA.exp MIN_CUDA.lib MIN_CUDA_DEBUG.exe MIN_CUDA_DEBUG.exp MIN_CUDA_DEBUG.lib
